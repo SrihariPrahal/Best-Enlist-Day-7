87e1ba46eda94bc18bbf1dbd9741321929201195
SrihariPrahal <75318418+SrihariPrahal@users.noreply.github.com>
2020-12-03 18:37:23 +0530

    Add files via upload

---
+++ addObject.js
@@ -0,0 +1,11 @@
+function addObject(name,age){
+    this.Name = name;
+    this.Age = age;
+    this.Nationality = nationality;
+}
+
+var student1 = new addObject[{'name':'srihari','age':19}];
+var student2 = new addObject[{'name':'mary','age':16}];
+
+student1.push({'nationality':'Indian'});
+student2.push({'nationality':'German'});
